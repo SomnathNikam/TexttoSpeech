d39e7f5a04f8562d70cbe02fef83d1f45507e24b
SOMNATH <somnathnikam2076@gmail.com>
2023-09-16 17:41:30 +0530

    TexttoSpeech added

--- app/src/main/java/com/example/asuper/MainActivity.kt
+++ app/src/main/java/com/example/asuper/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.asuper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+
+import com.example.asuper.databinding.ActivityMainBinding
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var tts:TextToSpeech
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnspeak.setOnClickListener{
+            tts = TextToSpeech(this, TextToSpeech.OnInitListener {
+                if (it == TextToSpeech.SUCCESS){
+                    tts.language = Locale.US
+                    tts.setSpeechRate(1.0f)
+                    tts.speak(binding.etmultitext.text.toString(),TextToSpeech.QUEUE_ADD,null)
+                }
+            })
+        }
+
+
     }
 }
